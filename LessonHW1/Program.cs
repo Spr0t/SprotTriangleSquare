66f07c4ed44d21a7cb324e33428632cd832da666
Dell <Dell@DESKTOP-B6FMJUN>
2022-03-17 14:42:00 +0200

    Добавьте файлы проекта.

---
+++ LessonHW1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LessonHW1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Чтоб найти площадь треугольника:");
+            Console.WriteLine("Введите значение первой стороны:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение второй стороны:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение третьей стороны:");
+            int c = int.Parse(Console.ReadLine());
+
+            int p = (a + b + c) / 2;
+
+            var s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            Console.WriteLine($"Площадь треугольника равна: {s} см2");
+            Console.ReadLine();
+
+        }
+    }
+}
